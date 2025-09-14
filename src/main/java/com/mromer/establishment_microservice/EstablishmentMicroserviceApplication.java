48038d5d309c40ad196e2179e13f69b46dbbd7f8
Manuel Rojas <manuelrojas202030799@cunoc.edu.gt>
2025-09-14 15:35:05 -0600

    chore: add base project

---
+++ src/main/java/com/mromer/establishment_microservice/EstablishmentMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.mromer.establishment_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstablishmentMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstablishmentMicroserviceApplication.class, args);
+	}
+
+}
